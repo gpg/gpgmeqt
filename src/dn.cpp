2473ad9fcafd957627f84cf59a830defdf9b17b6
Sune Vuorela <sune@vuorela.dk>
2025-03-18 14:43:06 +0100

    Improve distinguished name parser
    * src/dn.cpp (struct NameAndOID): Remove.
    (oidmap): Replace static vector with function returning the vector.
    (attributeNameForOID): Support OIDs prefixed with "OID." or "oid.".
    (struct DnPair, digitp, hexdigitp, xtoi_1, xtoi_2,
    trim_trailing_spaces): Remove.
    (removeLeadingSpaces, removeTrailingSpaces, xtoi, parseHexString): New.
    (parse_dn_part): Rewrite. Add support for values in double quotes.
    Improve error handling.
    (parse_dn(const unsigned char*)): Rename to...
    (parseString): ...this. Work on std::string_view instead of
    const char *. Change return type.
    (parse_dn(const QString &)): Work on std::string_view instead of
    QString. Change return type.
    (DN::DN(const QString &), DN::DN(const char *)): Pass std::string_view
    to parse_dn.
    
    * tests/CMakeLists.txt: Add new test.
    * tests/t-dn.cpp: New.
    
    --
    Backport changes from the poppler/okular copy of the distinguished name
    parser and also copy over and adapt the test suite from there.

--- src/dn.cpp
+++ src/dn.cpp
@@ -5,8 +5,9 @@
     Copyright (c) 2004 Klarälvdalens Datakonsult AB
     Copyright (c) 2016 by Bundesamt für Sicherheit in der Informationstechnik
     Software engineering by Intevation GmbH
-    Copyright (c) 2024 g10 Code GmbH
+    Copyright (c) 2023-2025 g10 Code GmbH
     Software engineering by Ingo Klöcker <dev@ingo-kloecker.de>
+    Software engineering by Sune Vuorela <sune@vuorela.dk>
 
     QGpgME is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License as
@@ -38,6 +39,8 @@
  #include "config.h"
 #endif
 
+// Note: part of this file is also duplicated in kde:okular.git and freedesktop:poppler.git
+
 #include "dn.h"
 
 #include <QByteArray>
@@ -46,40 +49,29 @@
 
 #include <vector>
 
-namespace
+static const std::vector<std::pair<std::string_view, std::string_view>> &oidmap()
 {
-struct NameAndOID {
-    const char *name;
-    const char *oid;
-};
-static const std::vector<NameAndOID> oidmap = {
+    static const std::vector<std::pair<std::string_view, std::string_view>> oidmap_ = {
+        // clang-format off
     // keep them ordered by oid:
     { "SP", "ST" }, // hack to show the Sphinx-required/desired SP for
     // StateOrProvince, otherwise known as ST or even S
-    { "NameDistinguisher", "0.2.262.1.10.7.20" },
-    { "EMAIL", "1.2.840.113549.1.9.1" },
-    { "SN", "2.5.4.4" },
-    { "SerialNumber", "2.5.4.5" },
-    { "T", "2.5.4.12" },
-    { "D", "2.5.4.13" },
-    { "BC", "2.5.4.15" },
-    { "ADDR", "2.5.4.16" },
-    { "PC", "2.5.4.17" },
-    { "GN", "2.5.4.42" },
-    { "Pseudo", "2.5.4.65" },
-};
+    {"NameDistinguisher", "0.2.262.1.10.7.20"   },
+    {"EMAIL",             "1.2.840.113549.1.9.1"},
+    {"CN",                "2.5.4.3"             },
+    {"SN",                "2.5.4.4"             },
+    {"SerialNumber",      "2.5.4.5"             },
+    {"T",                 "2.5.4.12"            },
+    {"D",                 "2.5.4.13"            },
+    {"BC",                "2.5.4.15"            },
+    {"ADDR",              "2.5.4.16"            },
+    {"PC",                "2.5.4.17"            },
+    {"GN",                "2.5.4.42"            },
+    {"Pseudo",            "2.5.4.65"            },
+        // clang-format on
+    };
+    return oidmap_;
 }
-
-static const char *attributeNameForOID(const char *oid)
-{
-    for (const auto &m : oidmap) {
-        if (qstricmp(oid, m.oid) == 0) {
-            return m.name;
-        }
-    }
-    return nullptr;
-}
-
 
 class QGpgME::DN::Private
 {
@@ -120,191 +112,283 @@ private:
     int mRefCount;
 };
 
-namespace
+namespace detail
 {
-struct DnPair {
-    char *key;
-    char *value;
-};
+
+static std::string_view removeLeadingSpaces(std::string_view view)
+{
+    auto pos = view.find_first_not_of(' ');
+    if (pos > view.size()) {
+        return {};
+    }
+    return view.substr(pos);
 }
 
-// copied from CryptPlug and adapted to work on DN::Attribute::List:
-
-#define digitp(p)   (*(p) >= '0' && *(p) <= '9')
-#define hexdigitp(a) (digitp (a)                     \
-                      || (*(a) >= 'A' && *(a) <= 'F')  \
-                      || (*(a) >= 'a' && *(a) <= 'f'))
-#define xtoi_1(p)   (*(p) <= '9'? (*(p)- '0'): \
-                     *(p) <= 'F'? (*(p)-'A'+10):(*(p)-'a'+10))
-#define xtoi_2(p)   ((xtoi_1(p) * 16) + xtoi_1((p)+1))
-
-static char *
-trim_trailing_spaces(char *string)
+static std::string_view removeTrailingSpaces(std::string_view view)
 {
-    char *p, *mark;
+    auto pos = view.find_last_not_of(' ');
+    if (pos > view.size()) {
+        return {};
+    }
+    return view.substr(0, pos + 1);
+}
 
-    for (mark = NULL, p = string; *p; p++) {
-        if (isspace(*p)) {
-            if (!mark) {
-                mark = p;
-            }
-        } else {
-            mark = NULL;
+static unsigned char xtoi(unsigned char c)
+{
+    if (c <= '9') {
+        return c - '0';
+    }
+    if (c <= 'F') {
+        return c - 'A' + 10;
+    }
+    return c - 'a' + 10;
+}
+
+static unsigned char xtoi(unsigned char first, unsigned char second)
+{
+    return 16 * xtoi(first) + xtoi(second);
+}
+// Parses a hex string into actual content
+static std::optional<std::string> parseHexString(std::string_view view)
+{
+    auto size = view.size();
+    if (size == 0 || (size % 2 == 1)) {
+        return std::nullopt;
+    }
+    // It is only supposed to be called with actual hex strings
+    // but this is just to be extra sure
+    auto endHex = view.find_first_not_of("1234567890abcdefABCDEF");
+    if (endHex != std::string_view::npos) {
+        return {};
+    }
+    std::string result;
+    result.reserve(size / 2);
+    for (size_t i = 0; i < (view.size() - 1); i += 2) {
+        result.push_back(xtoi(view[i], view[i + 1]));
+    }
+    return result;
+}
+
+static std::string_view attributeNameForOID(std::string_view oid)
+{
+    if (oid.substr(0, 4) == std::string_view {"OID."} || oid.substr(0, 4) == std::string_view {"oid."}) { // c++20 has starts_with. we don't have that yet.
+        oid.remove_prefix(4);
+    }
+    for (const auto &m : oidmap()) {
+        if (oid == m.second) {
+            return m.first;
         }
     }
-    if (mark) {
-        *mark = '\0';
-    }
-
-    return string;
+    return {};
 }
 
 /* Parse a DN and return an array-ized one.  This is not a validating
    parser and it does not support any old-stylish syntax; gpgme is
    expected to return only rfc2253 compatible strings. */
-static const unsigned char *
-parse_dn_part(DnPair *array, const unsigned char *string)
+static std::pair<std::optional<std::string_view>, std::pair<std::string, std::string>> parse_dn_part(std::string_view stringv)
 {
-    const unsigned char *s, *s1;
-    size_t n;
-    char *p;
-
-    /* parse attributeType */
-    for (s = string + 1; *s && *s != '='; s++)
-        ;
-    if (!*s) {
-        return NULL;    /* error */
+    std::pair<std::string, std::string> dnPair;
+    auto separatorPos = stringv.find_first_of('=');
+    if (separatorPos == 0 || separatorPos == std::string_view::npos) {
+        return {}; /* empty key */
     }
-    n = s - string;
-    if (!n) {
-        return NULL;    /* empty key */
-    }
-    p = (char *)malloc(n + 1);
 
-    memcpy(p, string, n);
-    p[n] = 0;
-    trim_trailing_spaces((char *)p);
+    std::string_view key = stringv.substr(0, separatorPos);
+    key = removeTrailingSpaces(key);
     // map OIDs to their names:
-    if (const char *name = attributeNameForOID(p)) {
-        free(p);
-        gpgrt_asprintf(&p, "%s", name);
+    if (auto name = attributeNameForOID(key); !name.empty()) {
+        key = name;
     }
-    array->key = p;
-    string = s + 1;
 
-    if (*string == '#') {
+    dnPair.first = std::string {key};
+    stringv = removeLeadingSpaces(stringv.substr(separatorPos + 1));
+    if (stringv.empty()) {
+        return {};
+    }
+
+    if (stringv.front() == '#') {
         /* hexstring */
-        string++;
-        for (s = string; hexdigitp(s); s++)
-          ;
-        n = s - string;
-        if (!n || (n & 1)) {
-            return NULL;    /* empty or odd number of digits */
+        stringv.remove_prefix(1);
+        auto endHex = stringv.find_first_not_of("1234567890abcdefABCDEF");
+        if (!endHex || (endHex % 2 == 1)) {
+            return {}; /* empty or odd number of digits */
         }
-        n /= 2;
-        array->value = p = (char *)malloc(n + 1);
-
-        for (s1 = string; n; s1 += 2, n--) {
-            *p++ = xtoi_2(s1);
+        auto value = parseHexString(stringv.substr(0, endHex));
+        if (!value.has_value()) {
+            return {};
         }
-        *p = 0;
-    } else {
-        /* regular v3 quoted string */
-        for (n = 0, s = string; *s; s++) {
-            if (*s == '\\') {
-                /* pair */
-                s++;
-                if (*s == ',' || *s == '=' || *s == '+'
-                        || *s == '<' || *s == '>' || *s == '#' || *s == ';'
-                        || *s == '\\' || *s == '\"' || *s == ' ') {
-                    n++;
-                } else if (hexdigitp(s) && hexdigitp(s + 1)) {
-                    s++;
-                    n++;
-                } else {
-                    return NULL;    /* invalid escape sequence */
+        stringv = stringv.substr(endHex);
+        dnPair.second = value.value();
+    } else if (stringv.front() == '"') {
+        stringv.remove_prefix(1);
+        std::string value;
+        bool stop = false;
+        while (!stringv.empty() && !stop) {
+            switch (stringv.front()) {
+            case '\\': {
+                if (stringv.size() < 2) {
+                    return {};
                 }
-            } else if (*s == '\"') {
-                return NULL;    /* invalid encoding */
-            } else if (*s == ',' || *s == '=' || *s == '+'
-                       || *s == '<' || *s == '>' || *s == '#' || *s == ';') {
+                if (stringv[1] == '"') {
+                    value.push_back('"');
+                    stringv.remove_prefix(2);
+                } else {
+                    // it is a bit unclear in rfc2253 if escaped hex chars should
+                    // be decoded inside quotes. Let's just forward the verbatim
+                    // for now
+                    value.push_back(stringv.front());
+                    value.push_back(stringv[1]);
+                    stringv.remove_prefix(2);
+                }
                 break;
-            } else {
-                n++;
+            }
+            case '"': {
+                stop = true;
+                stringv.remove_prefix(1);
+                break;
+            }
+            default: {
+                value.push_back(stringv.front());
+                stringv.remove_prefix(1);
+            }
             }
         }
-
-        array->value = p = (char *)malloc(n + 1);
-
-        for (s = string; n; s++, n--) {
-            if (*s == '\\') {
-                s++;
-                if (hexdigitp(s)) {
-                    *p++ = xtoi_2(s);
-                    s++;
-                } else {
-                    *p++ = *s;
+        if (!stop) {
+            // we have reached end of string, but never an actual ", so error out
+            return {};
+        }
+        dnPair.second = value;
+    } else {
+        std::string value;
+        bool stop = false;
+        bool lastAddedEscapedSpace = false;
+        while (!stringv.empty() && !stop) {
+            switch (stringv.front()) {
+            case '\\': //_escaping
+            {
+                stringv.remove_prefix(1);
+                if (stringv.empty()) {
+                    return {};
                 }
-            } else {
-                *p++ = *s;
+                switch (stringv.front()) {
+                case ',':
+                case '=':
+                case '+':
+                case '<':
+                case '>':
+                case '#':
+                case ';':
+                case '\\':
+                case '"':
+                case ' ': {
+                    if (stringv.front() == ' ') {
+                        lastAddedEscapedSpace = true;
+                    } else {
+                        lastAddedEscapedSpace = false;
+                    }
+                    value.push_back(stringv.front());
+                    stringv.remove_prefix(1);
+                    break;
+                }
+                default: {
+                    if (stringv.size() < 2) {
+                        // this should be double hex-ish, but isn't.
+                        return {};
+                    }
+                    if (std::isxdigit(stringv.front()) && std::isxdigit(stringv[1])) {
+                        lastAddedEscapedSpace = false;
+                        value.push_back(xtoi(stringv.front(), stringv[1]));
+                        stringv.remove_prefix(2);
+                        break;
+                    } else {
+                        // invalid escape
+                        return {};
+                    }
+                }
+                }
+                break;
+            }
+            case '"':
+                // unescaped " in the middle; not allowed
+                return {};
+            case ',':
+            case '=':
+            case '+':
+            case '<':
+            case '>':
+            case '#':
+            case ';': {
+                stop = true;
+                break; //
+            }
+            default:
+                lastAddedEscapedSpace = false;
+                value.push_back(stringv.front());
+                stringv.remove_prefix(1);
             }
         }
-        *p = 0;
+        if (lastAddedEscapedSpace) {
+            dnPair.second = value;
+        } else {
+            dnPair.second = std::string {removeTrailingSpaces(value)};
+        }
     }
-    return s;
+    return {stringv, dnPair};
 }
+}
+
+using Result = std::vector<std::pair<std::string, std::string>>;
 
 /* Parse a DN and return an array-ized one.  This is not a validating
    parser and it does not support any old-stylish syntax; gpgme is
    expected to return only rfc2253 compatible strings. */
-static QGpgME::DN::Attribute::List
-parse_dn(const unsigned char *string)
+static Result parseString(std::string_view string)
 {
-    if (!string) {
-        return QVector<QGpgME::DN::Attribute>();
-    }
-
-    QVector<QGpgME::DN::Attribute> result;
-    while (*string) {
-        while (*string == ' ') {
-            string++;
-        }
-        if (!*string) {
-            break;    /* ready */
+    Result result;
+    while (!string.empty()) {
+        string = detail::removeLeadingSpaces(string);
+        if (string.empty()) {
+            break;
         }
 
-        DnPair pair = { nullptr, nullptr };
-        string = parse_dn_part(&pair, string);
-        if (!string) {
-            goto failure;
+        auto [partResult, dnPair] = detail::parse_dn_part(string);
+        if (!partResult.has_value()) {
+            return {};
         }
-        if (pair.key && pair.value)
-            result.push_back(QGpgME::DN::Attribute(QString::fromUtf8(pair.key),
-                                                 QString::fromUtf8(pair.value)));
-        free(pair.key);
-        free(pair.value);
 
-        while (*string == ' ') {
-            string++;
+        string = partResult.value();
+        if (dnPair.first.size() && dnPair.second.size()) {
+            result.emplace_back(std::move(dnPair));
         }
-        if (*string && *string != ',' && *string != ';' && *string != '+') {
-            goto failure;    /* invalid delimiter */
+
+        string = detail::removeLeadingSpaces(string);
+        if (string.empty()) {
+            break;
         }
-        if (*string) {
-            string++;
+        switch (string.front()) {
+        case ',':
+        case ';':
+        case '+':
+            string.remove_prefix(1);
+            break;
+        default:
+            // some unexpected characters here
+            return {};
         }
     }
     return result;
-
-failure:
-    return QVector<QGpgME::DN::Attribute>();
 }
 
-static QVector<QGpgME::DN::Attribute>
-parse_dn(const QString &dn)
-{
-    return parse_dn((const unsigned char *)dn.toUtf8().data());
+QGpgME::DN::AttributeList parse_dn(std::string_view view) {
+    auto parsed = parseString(view);
+    QGpgME::DN::AttributeList list;
+    list.reserve(parsed.size());
+    for (auto& item : parsed) {
+        list.append(QGpgME::DN::Attribute(QString::fromStdString(item.first), QString::fromStdString(item.second)));
+    }
+    return  list;
 }
+
 
 static QString dn_escape(const QString &s)
 {
@@ -394,7 +478,7 @@ QGpgME::DN::DN(const QString &dn)
 {
     d = new Private();
     d->ref();
-    d->attributes = parse_dn(dn);
+    d->attributes = parse_dn(dn.toStdString());
 }
 
 QGpgME::DN::DN(const char *utf8DN)
@@ -402,7 +486,7 @@ QGpgME::DN::DN(const char *utf8DN)
     d = new Private();
     d->ref();
     if (utf8DN) {
-        d->attributes = parse_dn((const unsigned char *)utf8DN);
+        d->attributes = parse_dn(std::string_view(utf8DN, strlen(utf8DN)));
     }
 }
 
